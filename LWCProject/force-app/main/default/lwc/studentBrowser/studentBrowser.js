f33fbd01ba73f66b4fbe4b9f2098a6a82eba45d1
Pramod Jana <32437635+PramodJana@users.noreply.github.com>
2020-11-23 10:32:40 +0530

    implemented combobox in lwc project

--- LWCProject/force-app/main/default/lwc/studentBrowser/studentBrowser.js
+++ LWCProject/force-app/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,38 +1,49 @@
-import { LightningElement, track, wire } from 'lwc';
-import {CurrentPageReference } from 'lightning/navigation'
+import { api, LightningElement, track, wire } from 'lwc';
 import getStudents from '@salesforce/apex/StudentController.getStudents';
-import {fireEvent} from 'c/pubsub';
-
+import { CurrentPageReference } from 'lightning/navigation'
+import { fireEvent } from 'c/pubsub';
+//import {refreshApex} from '@salesforce/apex';
+//import { idr } from 'c/studentFilter';
+import getAccounts from '@salesforce/apex/StudentController.getAccounts';
 export default class StudentBrowser extends LightningElement {
-
+    @track studentList=[];
+    @track account='';
+    @track accountId='';
+    //_wiredServerResponse;
+    onfilterchange(event){
+        
+        this.accountId=event.detail.accId;
+        alert(this.accountId);       
+        //refreshApex(this._wiredServerResponse);
+    }
     @wire(CurrentPageReference) pageRef;
-    
-    @track studentList = [];
-    accountId = '';
-
-    @wire(getStudents,{accId : '$accountId'})// making wire function to execute dynamically
+    @wire(getStudents,{accId: '$accountId'}) //making wire function to execute dynamically
     wiredGetStudents({data,error}){
+        
+        //this._wiredServerResponse=data;
         if(data){
+            this.studentList=[];
             data.forEach(std => {
                 this.studentList.push({
-                    Name : std.FirstName+' '+std.LastName,
-                    Title : std.Title,
-                    Phone : std.Phone,
-                    Email : std.Email,
-                    PhotoUrl : '/services/images/photo/00300FakePictId',
-                    Id : std.Id,
-                });
+                    Name: std.FirstName+' '+std.LastName,
+                    Title: std.Title,
+                    Phone: std.Phone,
+                    Email: std.Email,
+                    PhotoUrl: '/services/images/photo/00380FakePictId',
+                    Id: std.Id
+                })
             });
         }
-        else if(error){
-            //code
-        }
-    }// end of wired
-
-    handleTileClick(event){
-        this.selectedStudentId = event.detail.stdId;
-        alert('browser has received information '+this.selectedStudentId);
-
-        fireEvent(this.pageRef,'studentChange',{stdId: this.selectedStudentId});
+       
+        
     }
-}//end of class
+    handleTileClick(event){
+        this.selectedStudentId=event.detail.stdId;
+        alert('Tiles have recieved information '+this.selectedStudentId);
+
+        fireEvent(this.pageRef, 'studentChange',{stdId : this.selectedStudentId});
+    }
+    handleStudentfilter(event){
+        this.accountId = event.detail.accId;
+    }
+}
--- LWCProject/force-app/main/default/lwc/studentFilter/studentFilter.js
+++ LWCProject/force-app/main/default/lwc/studentFilter/studentFilter.js
@@ -1,3 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import getAccounts from '@salesforce/apex/StudentController.getAccounts';
 
-export default class StudentFilter extends LightningElement {}
+export default class StudentFilter extends LightningElement {
+    @track accCombList = [];
+    @track selectedAccountId = '';
+
+    @wire(getAccounts)
+    wiredGetAccounts({data,error}){
+       this.accCombList = []; //good practice
+       if(data){
+            data.forEach(acc=>{
+                this.accCombList.push(
+                    {
+                        label: acc.Name,
+                        value: acc.Id
+                    }
+                );
+             //console.log(JSON.stringify(data));  
+            // console.log(JSON.stringify(this.accCombList));  
+            });
+       }else if (error){ 
+           //..
+       }
+    }
+
+    onAccountChange(event){
+        this.selectedAccountId = event.target.value;
+        this.notifyParent();
+    }
+
+    notifyParent(){
+        const eventRef = new CustomEvent("studentfilter",
+                                    {detail : 
+                                              { 
+                                                accId: this.selectedAccountId 
+                                              }
+                                     });
+        this.dispatchEvent(eventRef);                             
+    }
+
+}
+
+
